7389f8ab756688316b72648e3c0d688bd1ead385
Ludson96 <ludson_ps27@hotmail.com>
2022-12-04 15:50:28 -0300

    add migration create-users

---
+++ src/migrations/20221204183529-create-blog_posts.js
@@ -0,0 +1,38 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('blog_posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+      },
+      content: { 
+        type: Sequelize.STRING,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        field: 'user_id',
+        references: {
+          model: 'users',
+          key: 'id',
+        }
+      },
+      published: {
+        type: Sequelize.DATE,
+      },
+      updated: {
+        type: Sequelize.DATE,
+      },
+    })
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+   return queryInterface.dropTable('blog_posts');
+  }
+};
